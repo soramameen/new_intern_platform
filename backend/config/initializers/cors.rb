9bff10f5eb5392f4e298137580a910d90d2fdd6b
soramameen <sorananonaomasa.sora@gmail.com>
2025-04-03 11:07:23 +0900

    CORS更新

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,7 +1,9 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.use Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://new-intern-platform-git-main-soramameens-projects.vercel.app'
+    origins 'http://localhost:3000', 
+            'https://new-intern-platform-git-main-soramameens-projects.vercel.app',
+            'https://new-intern-platform.vercel.app'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
