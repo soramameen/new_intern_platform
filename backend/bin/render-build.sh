2187299a7ac88f819f5238a6a098ac44327c57b9
soramameen <sorananonaomasa.sora@gmail.com>
2025-04-03 11:41:10 +0900

    seeds2

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -7,7 +7,9 @@ bundle install
 # bundle exec rake assets:precompile
 # bundle exec rake assets:clean
 # seeds用コメントアウト
+
+# 安全なマイグレーション
 bundle exec rails db:migrate
 
-# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
-# bundle exec rake db:seed 
+# 条件付きシーディング（必要な場合のみ）
+bundle exec rails db:seed
