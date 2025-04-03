4cc63a03c2f54f8f38e11c59dd7ce6b525208044
soramameen <sorananonaomasa.sora@gmail.com>
2025-04-03 11:35:44 +0900

    通常に戻す

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -7,7 +7,7 @@ bundle install
 # bundle exec rake assets:precompile
 # bundle exec rake assets:clean
 # seeds用コメントアウト
-#bundle exec rails db:migrate
+bundle exec rails db:migrate
 
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
-bundle exec rake db:seed 
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+# bundle exec rake db:seed 
